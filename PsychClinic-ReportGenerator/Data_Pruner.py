6f71b6f87eb241a06cefb341dea1af797ee8f488
plasmaaShark <shaylinsmith07@gmail.com>
2025-03-17 11:11:39 -0700

    Update Data_Pruner.py

--- PsychClinic-ReportGenerator/Data_Pruner.py
+++ PsychClinic-ReportGenerator/Data_Pruner.py
@@ -580,7 +580,7 @@ def add_radar(data):
 
 def add_components(data):
     options = {}
-    name = ['temperament', 'self-concept', 'goals', 'total_survey']
+    name = ['total_survey', 'temperament', 'self-concept', 'goals']
     column_index = ['496_1', '496_2', '496_3', '496_4']
     for index in column_index:
         column_name = f'Q{index}'
